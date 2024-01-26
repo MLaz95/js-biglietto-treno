2ae9cb64422a902275f39144281ef3e301af5c87
marco <lazzari.marco.ml@gmail.com>
2024-01-26 16:42:33 +0100

    aggiunto controllo che l'user stia inserendo numeri

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const km = prompt("Quanto dista la tua destinazione in chilometri?");
-const age = prompt ("Quanti anni hai?");
+const km = Number(prompt("Quanto dista la tua destinazione in chilometri?"));
+const age = Number(prompt ("Quanti anni hai?"));
 
-let price = Number(km) * 0.21
+if(Number.isNaN(km) || Number.isNaN(age)){
+    document.getElementById("ticket-price").innerHTML = "Non posso calcolare il prezzo del tuo biglietto con questi parametri. Ricarica la pagina per riprovare.";
 
-if (age < 18){
+} else{
+let price = km * 0.21
+
+    if (age < 18){
     price *= 1 - 0.2;
 
-} else if (age > 65){
+    } else if (age > 65){
     price *= 1 - 0.4;
+    }
+
+    document.getElementById("ticket-price").innerHTML = `Il prezzo del tuo biglietto è ${price.toFixed(2)}€.`;
 }
 
-document.getElementById("ticket-price").innerHTML = `Il prezzo del tuo biglietto è ${price.toFixed(2)}€.`;
+
+    
 
