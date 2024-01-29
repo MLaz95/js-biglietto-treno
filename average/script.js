2afa4033bc908edb088e2bf5d809ab47690301b8
marco <lazzari.marco.ml@gmail.com>
2024-01-29 14:03:04 +0100

    aggiunto check user input e stile; average completato

--- average/script.js
+++ average/script.js
@@ -2,5 +2,11 @@ const firstValue = Number(prompt("I will calculate the average of three values. 
 const secondValue = Number(prompt("Insert the second value."));
 const thirdValue = Number(prompt("Insert the third value."));
 
-const averageValue = (firstValue + secondValue + thirdValue) / 3;
-document.getElementById("average").innerHTML = averageValue.toFixed(2);
+if(Number.isNaN(firstValue) || Number.isNaN(secondValue) || Number.isNaN(thirdValue)){
+    document.getElementById("average").innerHTML = "I cannot average the provided values. Refresh the page to retry.";
+
+} else{
+    const averageValue = (firstValue + secondValue + thirdValue) / 3;
+    document.getElementById("intro").innerHTML = `The average of ${firstValue}, ${secondValue}, and ${thirdValue} is`
+    document.getElementById("average").innerHTML = averageValue.toFixed(2);
+}
