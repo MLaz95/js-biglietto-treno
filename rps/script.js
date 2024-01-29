66df74401f0166b99667431366a1fe3e227d847d
marco <lazzari.marco.ml@gmail.com>
2024-01-29 15:23:52 +0100

    aggiunto stile; gioco rps completato

--- rps/script.js
+++ rps/script.js
@@ -1,9 +1,11 @@
 let playerHand = prompt("Let's play! choose 'rock' 'paper' or 'scissors'")
-// let playerHand = "scissors";
+// let playerHand = "paper";
+document.getElementById("player").innerHTML = `You chose ${playerHand}!`;
+
 
 
 // script picks random hand value
-// let rndHand = "paper";
+// let rndHand = "rock";
 
 let rndHand = Math.floor(Math.random() * 3) + 1;
 
@@ -15,14 +17,23 @@ if (rndHand == 1){
     rndHand = "scissors";
 }
 console.log(rndHand);
+document.getElementById("gon").innerHTML = `Gon picked ${rndHand}!`;
+
 
 // determines who won
 if (playerHand != "rock" && playerHand != "paper" && playerHand != "scissors"){
-    document.getElementById("result").innerHTML = "That's not allowed! Refresh if you actually feel like playing."
+    document.getElementById("result").innerHTML = "I don't think that's allowed...are you sure you wanna play? (Press F5)"
+    document.getElementById("gon-face").setAttribute("src", "./img/gon_null.jpeg")
+    
 } else if (playerHand == rndHand){
-    document.getElementById("result").innerHTML = "It's a tie! Let's play again!"
+    document.getElementById("result").innerHTML = "It's a tie! Let's go again! (Press F5)"
+    document.getElementById("gon-face").setAttribute("src", "./img/gon_tie.jpeg")
+
 } else if (playerHand == "rock" && rndHand == "paper" || playerHand == "paper" && rndHand == "scissors" || playerHand == "scissors" && rndHand == "rock"){
-    document.getElementById("result").innerHTML = "Hehe gotcha, I win. Wanna try again?"
+    document.getElementById("result").innerHTML = "Hehe gotcha, I win. Wanna try again? (Press F5)"
+    document.getElementById("gon-face").setAttribute("src", "./img/gon_win.jpeg")
+
 } else {
-    document.getElementById("result").innerHTML = "Aaarrhg you won! Again, one more time!"
+    document.getElementById("result").innerHTML = "Aaarrhg you won! Again, one more time! (Press F5)"
+    document.getElementById("gon-face").setAttribute("src", "./img/gon_lose.jpeg")
 }
