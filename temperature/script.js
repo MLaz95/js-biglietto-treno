ed0f403978a4f6b7d546529ea92e192bbec01a53
marco <lazzari.marco.ml@gmail.com>
2024-01-29 12:24:31 +0100

    aggiunta verifica che l'utente inserisca un numero

--- temperature/script.js
+++ temperature/script.js
@@ -1,4 +1,10 @@
-const tempC = prompt("Insert a temperature value in °C to convert it to °F");
+const tempC = Number(prompt("Insert a temperature value in °C to convert it to °F"));
 
-const tempF = tempC * (9 / 5) + 32;
-document.getElementById('temp').innerHTML = `The temperature is ${tempF}°F`
+if (Number.isNaN(tempC)){
+    document.getElementById('temp').innerHTML = `I cannot convert that value. Refresh the page to retry.`;
+
+} else {
+    const tempF = tempC * (9 / 5) + 32;
+    document.getElementById('temp').innerHTML = `The temperature is ${tempF}°F`
+}
+
